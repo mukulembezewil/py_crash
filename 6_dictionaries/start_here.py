d2747d76c1a0e7a2d52239a468b35e36acf112d8
mukulembezewil <wilmukulembeze@gmail.com>
2024-01-12 11:05:27 +0300

    starting with dictionaries

---
+++ 6_dictionaries/start_here.py
@@ -0,0 +1,2 @@
+# A Python dictionary is a data structure that stores a collection of Key-value pairs. Each key-value pair maps the key to its associated value. Dictionaries are created using curly braces and can be defined as objects with the data type 'dict'. They are ordered, changeable and do not allow duplicates. Dictionaries are commonly used to store and retrieve data based on a unique key.
+
