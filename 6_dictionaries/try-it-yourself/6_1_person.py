474f83aea3392c53cad915118708d93160213bc9
mukulembezewil <wilmukulembeze@gmail.com>
2024-01-15 15:38:20 +0300

    Complete exercise one.

--- 6_dictionaries/try-it-yourself/6_1_person.py
+++ 6_dictionaries/try-it-yourself/6_1_person.py
@@ -1,2 +1,15 @@
 # Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
 
+person = {
+	"first_name": "Governor",
+	"last_name": "Oz",
+	"age": 10,
+	"city": "Tokyo",
+	}
+
+print(person.get("first_name"))
+print(person.get("last_name"))
+print(person.get("age"))
+print(person.get("city"))
+
+
