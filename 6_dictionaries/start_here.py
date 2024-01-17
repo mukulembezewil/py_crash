c70b1c2dfb8eb413ebe6f0458d936b6a817d7873
mukulembezewil <wilmukulembeze@gmail.com>
2024-01-17 08:18:43 +0300

    more on looping through dictionaries

--- 6_dictionaries/start_here.py
+++ 6_dictionaries/start_here.py
@@ -98,3 +98,16 @@ print(favorite_languages.get("Antoine"))
 
 # If you leave out the second argument in the call to get() and the key doesn’t exist, Python will return the value None. The special value None means “no value exists.” This is not an error: it’s a special value meant to indicate the absence of a value. You’ll see more uses for None in Chapter 8.
 print('_______________________\n')
+
+
+# LOOPING THROUGH A DICTIONARY
+
+# Several different ways exist to loop through dictionaries. You can loop through all of a dictionary’s key-value pairs, through its keys, or through its values.
+
+	# Looping through all key value pairs:
+
+user_0 = {
+	"username": "efermi",
+	"first": "henrico",
+	"last": "efermi",
+	}
